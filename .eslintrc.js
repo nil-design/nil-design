db9d54838af04e5d863aaeb17363364b7ff949c9
Morilence <1280659615@qq.com>
2022-10-31 14:51:58 +0800

    chore: make all .js scripts support commonjs import syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,12 +35,12 @@ module.exports = {
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
 
         "@typescript-eslint/no-unused-vars": "warn",
-        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-explicit-any": "off",
     },
 
     overrides: [
         {
-            files: ["scripts/**/*.js"],
+            files: ["*.js"],
             rules: {
                 "@typescript-eslint/no-var-requires": "off",
             },
