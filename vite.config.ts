03c6af56ba32da835a8fb9526490d76f2e6fd962
Morilence <1280659615@qq.com>
2022-10-31 21:23:31 +0800

    build: completed configurations & scripts for docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+const ROOT = "packages/docs";
 export default defineConfig({
+    root: `${ROOT}/src`,
     plugins: [react()],
+    server: {
+        fs: {
+            strict: true,
+        },
+    },
+    /**
+     * using absolute paths can prevent the structure of "packages/docs/src" from being built
+     */
+    build: {
+        outDir: resolve(ROOT, "dist"),
+        rollupOptions: {
+            input: resolve(ROOT, "src/index.html"),
+        },
+    },
 });
