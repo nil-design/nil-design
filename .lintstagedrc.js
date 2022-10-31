55c9077b3be45ff48b0831f4b05d90bc6cf432c0
Morilence <1280659615@qq.com>
2022-10-31 16:18:48 +0800

    chore: add support for stylelint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "*.{js,ts,cjs,mjs}": ["eslint --fix", "prettier --write"],
-    "*.{json,json5,jsonc}": ["prettier --write"],
-    "*.{yml,yaml}": ["prettier --write"],
+    "*.{js,cjs,mjs,jsx,ts,tsx}": ["eslint --fix"],
+    "*.{css,less}": ["stylelint --fix"],
+    "*.{json,json5,jsonc,yaml,yml}": ["prettier --write"],
     "*.{md,markdown}": ["markdownlint-cli2", "prettier --write"],
 };
---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ["stylelint-config-standard", "stylelint-config-recess-order", "stylelint-config-prettier"],
+    plugins: ["stylelint-prettier"],
+
+    customSyntax: "postcss-less",
+    rules: {
+        "prettier/prettier": true,
+    },
+};
