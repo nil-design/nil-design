b31109b6047ee14289caf29771b7142b795fa4f6
Morilence <1280659615@qq.com>
2022-10-31 21:18:53 +0800

    build: configured the building of each core subpkg

--- rollup.config.js
+++ rollup.config.js
@@ -9,64 +9,67 @@ const autoprefixer = require("autoprefixer");
 const { getRootPath, getSubPkgs, getBrowsersList } = require("./scripts/lib");
 
 const { NODE_ENV } = process.env;
+const ignoredDirs = ["docs"]; // subpackages that do not be bundled by rollup
 
 module.exports = defineConfig(
-    getSubPkgs().map(({ dirName: subPkgName, dirPath }) => {
-        const subPkgRelPath = relative(getRootPath(), dirPath);
-        /* common config */
-        let config = {
-            input: `${subPkgRelPath}/src/index.ts`,
-            output: [
-                {
-                    file: `${subPkgRelPath}/dist/index.js`,
-                    format: "cjs",
-                },
-                {
-                    file: `${subPkgRelPath}/dist/index.esm.js`,
-                    format: "esm",
-                },
-            ],
-            plugins: [
-                json(),
-                commonjs(),
-                typescript({
-                    tsconfig: "tsconfig.json",
-                    tsconfigOverride: {
-                        compilerOptions: {
-                            rootDir: `${subPkgRelPath}/src`,
-                        },
-                        include: ["types/**/*.d.ts", `${subPkgRelPath}/src/**/*.ts`],
-                        exclude: [`${subPkgRelPath}/src/**/*.test.ts`],
+    getSubPkgs()
+        .filter(({ dirName }) => !ignoredDirs.includes(dirName))
+        .map(({ dirName: subPkgName, dirPath }) => {
+            const subPkgRelPath = relative(getRootPath(), dirPath);
+            /* common configs */
+            let config = {
+                input: `${subPkgRelPath}/src/index.ts`,
+                output: [
+                    {
+                        file: `${subPkgRelPath}/dist/index.js`,
+                        format: "cjs",
                     },
-                }),
-                terser(),
-            ],
-        };
+                    {
+                        file: `${subPkgRelPath}/dist/index.esm.js`,
+                        format: "esm",
+                    },
+                ],
+                plugins: [
+                    json(),
+                    commonjs(),
+                    typescript({
+                        tsconfig: "tsconfig.json",
+                        tsconfigOverride: {
+                            compilerOptions: {
+                                rootDir: `${subPkgRelPath}/src`,
+                            },
+                            include: ["types/**/*.d.ts", `${subPkgRelPath}/src/**/*.ts`],
+                            exclude: [`${subPkgRelPath}/src/**/*.test.ts`],
+                        },
+                    }),
+                    terser(),
+                ],
+            };
 
-        /* add each as needed */
-        switch (subPkgName) {
-            case "components":
-                config.plugins.push(
-                    ...[
-                        postcss({
-                            extract: false,
-                            minimize: NODE_ENV === "production",
-                            plugins: [
-                                autoprefixer({
-                                    env: NODE_ENV,
-                                    overrideBrowserslist: getBrowsersList(NODE_ENV),
-                                    grid: "autoplace",
-                                }),
-                            ],
-                            extensions: [".css", ".less"],
-                        }),
-                    ]
-                );
-                break;
-            default:
-                break;
-        }
+            /* add each as needed */
+            switch (subPkgName) {
+                case "components":
+                    config.plugins.push(
+                        ...[
+                            postcss({
+                                extract: false,
+                                minimize: NODE_ENV === "production",
+                                plugins: [
+                                    autoprefixer({
+                                        env: NODE_ENV,
+                                        overrideBrowserslist: getBrowsersList(NODE_ENV),
+                                        grid: "autoplace",
+                                    }),
+                                ],
+                                extensions: [".css", ".less"],
+                            }),
+                        ]
+                    );
+                    break;
+                default:
+                    break;
+            }
 
-        return config;
-    })
+            return config;
+        })
 );
