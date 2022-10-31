2814134cee821282df0b1b26514683a0fd8625eb
Morilence <1280659615@qq.com>
2022-10-31 15:39:22 +0800

    build: created configuration file

---
+++ rollup.config.js
@@ -0,0 +1,72 @@
+const { relative } = require("node:path");
+const { defineConfig } = require("rollup");
+const json = require("@rollup/plugin-json");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("rollup-plugin-typescript2");
+const postcss = require("rollup-plugin-postcss");
+const { terser } = require("rollup-plugin-terser");
+const autoprefixer = require("autoprefixer");
+const { getRootPath, getSubPkgs, getBrowsersList } = require("./scripts/lib");
+
+const { NODE_ENV } = process.env;
+
+module.exports = defineConfig(
+    getSubPkgs().map(({ dirName: subPkgName, dirPath }) => {
+        const subPkgRelPath = relative(getRootPath(), dirPath);
+        /* common config */
+        let config = {
+            input: `${subPkgRelPath}/src/index.ts`,
+            output: [
+                {
+                    file: `${subPkgRelPath}/dist/index.js`,
+                    format: "cjs",
+                },
+                {
+                    file: `${subPkgRelPath}/dist/index.esm.js`,
+                    format: "esm",
+                },
+            ],
+            plugins: [
+                json(),
+                commonjs(),
+                typescript({
+                    tsconfig: "tsconfig.json",
+                    tsconfigOverride: {
+                        compilerOptions: {
+                            rootDir: `${subPkgRelPath}/src`,
+                        },
+                        include: ["types/**/*.d.ts", `${subPkgRelPath}/src/**/*.ts`],
+                        exclude: [`${subPkgRelPath}/src/**/*.test.ts`],
+                    },
+                }),
+                terser(),
+            ],
+        };
+
+        /* add each as needed */
+        switch (subPkgName) {
+            case "components":
+                config.plugins.push(
+                    ...[
+                        postcss({
+                            extract: false,
+                            minimize: NODE_ENV === "production",
+                            plugins: [
+                                autoprefixer({
+                                    env: NODE_ENV,
+                                    overrideBrowserslist: getBrowsersList(NODE_ENV),
+                                    grid: "autoplace",
+                                }),
+                            ],
+                            extensions: [".css", ".less"],
+                        }),
+                    ]
+                );
+                break;
+            default:
+                break;
+        }
+
+        return config;
+    })
+);
