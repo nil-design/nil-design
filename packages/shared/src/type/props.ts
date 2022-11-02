6da3abcd4aea828e36fafe5f1536a6c17263542c
Morilence <1280659615@qq.com>
2022-11-02 23:38:47 +0800

    feat: added props for some native html elements

--- packages/shared/src/type/props.ts
+++ packages/shared/src/type/props.ts
@@ -3,16 +3,40 @@ import {
     AreaHTMLAttributes,
     AudioHTMLAttributes,
     BaseHTMLAttributes,
+    BlockquoteHTMLAttributes,
     ButtonHTMLAttributes,
     CanvasHTMLAttributes,
     DataHTMLAttributes,
+    DelHTMLAttributes,
     DialogHTMLAttributes,
     EmbedHTMLAttributes,
     FieldsetHTMLAttributes,
     FormHTMLAttributes,
     HTMLAttributes,
+    HtmlHTMLAttributes,
+    IframeHTMLAttributes,
+    ImgHTMLAttributes,
+    InputHTMLAttributes,
+    InsHTMLAttributes,
+    LabelHTMLAttributes,
+    LinkHTMLAttributes,
+    MapHTMLAttributes,
+    MetaHTMLAttributes,
+    MeterHTMLAttributes,
+    ObjectHTMLAttributes,
+    OlHTMLAttributes,
+    OptgroupHTMLAttributes,
+    OptionHTMLAttributes,
+    OutputHTMLAttributes,
+    ProgressHTMLAttributes,
+    QuoteHTMLAttributes,
+    ScriptHTMLAttributes,
+    SelectHTMLAttributes,
+    SourceHTMLAttributes,
+    StyleHTMLAttributes,
 } from "react";
 
+export type HTMLElementProps = HTMLAttributes<HTMLElement>;
 /** <a> */
 export type HTMLAnchorProps = AnchorHTMLAttributes<HTMLAnchorElement>;
 /** <area> */
@@ -21,20 +45,91 @@ export type HTMLAreaProps = AreaHTMLAttributes<HTMLAreaElement>;
 export type HTMLAudioProps = AudioHTMLAttributes<HTMLAudioElement>;
 /** <base> */
 export type HTMLBaseProps = BaseHTMLAttributes<HTMLBaseElement>;
+/** <blockquote> */
+export type HTMLBlockquoteProps = BlockquoteHTMLAttributes<HTMLQuoteElement>;
+/** <body> */
 export type HTMLBodyProps = HTMLAttributes<HTMLBodyElement>;
+/** <br> */
 export type HTMLBRProps = HTMLAttributes<HTMLBRElement>;
+/** <button> */
 export type HTMLButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+/** <canvas>  */
 export type HTMLCanvasProps = CanvasHTMLAttributes<HTMLCanvasElement>;
+/** <data> */
 export type HTMLDataProps = DataHTMLAttributes<HTMLDataElement>;
+/** <datalist> */
 export type HTMLDataListProps = HTMLAttributes<HTMLDataListElement>;
+/** <del> */
+export type HTMLDelProps = DelHTMLAttributes<HTMLModElement>;
+/** <dialog> */
 export type HTMLDialogProps = DialogHTMLAttributes<HTMLDialogElement>;
+/** <div> */
 export type HTMLDivProps = HTMLAttributes<HTMLDivElement>;
+/** <dl> */
 export type HTMLDListProps = HTMLAttributes<HTMLDListElement>;
-export type HTMLElementProps = HTMLAttributes<HTMLElement>;
+/** <embed> */
 export type HTMLEmbedProps = EmbedHTMLAttributes<HTMLEmbedElement>;
+/** <fieldset> */
 export type HTMLFieldSetProps = FieldsetHTMLAttributes<HTMLFieldSetElement>;
+/** <form> */
 export type HTMLFormProps = FormHTMLAttributes<HTMLFormElement>;
-export type HTMLHRProps = HTMLAttributes<HTMLHRElement>;
+/** <head> */
 export type HTMLHeadProps = HTMLAttributes<HTMLElement>;
 /** <h1> - <h6> */
 export type HTMLHeadingProps = HTMLAttributes<HTMLHeadingElement>;
+/** <hr> */
+export type HTMLHRProps = HTMLAttributes<HTMLHRElement>;
+/** <html> */
+export type HTMLHtmlProps = HtmlHTMLAttributes<HTMLHtmlElement>;
+/** <iframe> */
+export type HTMLIFrameProps = IframeHTMLAttributes<HTMLIFrameElement>;
+/** <img> */
+export type HTMLImageProps = ImgHTMLAttributes<HTMLImageElement>;
+/** <input> */
+export type HTMLInputProps = InputHTMLAttributes<HTMLInputElement>;
+/** <ins> */
+export type HTMLInsProps = InsHTMLAttributes<HTMLModElement>;
+/** <label> */
+export type HTMLLabelProps = LabelHTMLAttributes<HTMLLabelElement>;
+/** <legend> */
+export type HTMLLegendProps = HTMLAttributes<HTMLLegendElement>;
+/** <link> */
+export type HTMLLinkProps = LinkHTMLAttributes<HTMLLinkElement>;
+/** <map> */
+export type HTMLMapProps = MapHTMLAttributes<HTMLMapElement>;
+/** <meta> */
+export type HTMLMetaProps = MetaHTMLAttributes<HTMLMetaElement>;
+/** <meter> */
+export type HTMLMeterProps = MeterHTMLAttributes<HTMLMeterElement>;
+/** <object> */
+export type HTMLObjectProps = ObjectHTMLAttributes<HTMLObjectElement>;
+/** <ol> */
+export type HTMLOListProps = OlHTMLAttributes<HTMLOListElement>;
+/** <optgroup> */
+export type HTMLOptGroupProps = OptgroupHTMLAttributes<HTMLOptGroupElement>;
+/** <option> */
+export type HTMLOptionProps = OptionHTMLAttributes<HTMLOptionElement>;
+/** <output> */
+export type HTMLOutputProps = OutputHTMLAttributes<HTMLOutputElement>;
+/** <p> */
+export type HTMLParagraphProps = HTMLAttributes<HTMLParagraphElement>;
+/** <picture> */
+export type HTMLPictureProps = HTMLAttributes<HTMLElement>;
+/** <pre> */
+export type HTMLPreProps = HTMLAttributes<HTMLPreElement>;
+/** <progress> */
+export type HTMLProgressProps = ProgressHTMLAttributes<HTMLProgressElement>;
+/** <q> */
+export type HTMLQuoteProps = QuoteHTMLAttributes<HTMLQuoteElement>;
+/** <script> */
+export type HTMLScriptProps = ScriptHTMLAttributes<HTMLScriptElement>;
+/** <section> */
+export type HTMLSectionProps = HTMLAttributes<HTMLElement>;
+/** <select> */
+export type HTMLSelectProps = SelectHTMLAttributes<HTMLSelectElement>;
+/** <source> */
+export type HTMLSourceProps = SourceHTMLAttributes<HTMLSourceElement>;
+/** <span> */
+export type HTMLSpanProps = HTMLAttributes<HTMLSpanElement>;
+/** <style> */
+export type HTMLStyleProps = StyleHTMLAttributes<HTMLStyleElement>;
