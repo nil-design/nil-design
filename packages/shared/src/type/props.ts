79a25e1e17468848c46845bf7d6e10ffeea123c0
Morilence <1280659615@qq.com>
2022-11-02 21:57:19 +0800

    feat: defined props for some native html elements

---
+++ packages/shared/src/type/props.ts
@@ -0,0 +1,40 @@
+import {
+    AnchorHTMLAttributes,
+    AreaHTMLAttributes,
+    AudioHTMLAttributes,
+    BaseHTMLAttributes,
+    ButtonHTMLAttributes,
+    CanvasHTMLAttributes,
+    DataHTMLAttributes,
+    DialogHTMLAttributes,
+    EmbedHTMLAttributes,
+    FieldsetHTMLAttributes,
+    FormHTMLAttributes,
+    HTMLAttributes,
+} from "react";
+
+/** <a> */
+export type HTMLAnchorProps = AnchorHTMLAttributes<HTMLAnchorElement>;
+/** <area> */
+export type HTMLAreaProps = AreaHTMLAttributes<HTMLAreaElement>;
+/** <audio> */
+export type HTMLAudioProps = AudioHTMLAttributes<HTMLAudioElement>;
+/** <base> */
+export type HTMLBaseProps = BaseHTMLAttributes<HTMLBaseElement>;
+export type HTMLBodyProps = HTMLAttributes<HTMLBodyElement>;
+export type HTMLBRProps = HTMLAttributes<HTMLBRElement>;
+export type HTMLButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+export type HTMLCanvasProps = CanvasHTMLAttributes<HTMLCanvasElement>;
+export type HTMLDataProps = DataHTMLAttributes<HTMLDataElement>;
+export type HTMLDataListProps = HTMLAttributes<HTMLDataListElement>;
+export type HTMLDialogProps = DialogHTMLAttributes<HTMLDialogElement>;
+export type HTMLDivProps = HTMLAttributes<HTMLDivElement>;
+export type HTMLDListProps = HTMLAttributes<HTMLDListElement>;
+export type HTMLElementProps = HTMLAttributes<HTMLElement>;
+export type HTMLEmbedProps = EmbedHTMLAttributes<HTMLEmbedElement>;
+export type HTMLFieldSetProps = FieldsetHTMLAttributes<HTMLFieldSetElement>;
+export type HTMLFormProps = FormHTMLAttributes<HTMLFormElement>;
+export type HTMLHRProps = HTMLAttributes<HTMLHRElement>;
+export type HTMLHeadProps = HTMLAttributes<HTMLElement>;
+/** <h1> - <h6> */
+export type HTMLHeadingProps = HTMLAttributes<HTMLHeadingElement>;
