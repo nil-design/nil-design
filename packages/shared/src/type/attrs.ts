7a070cff292f0ea2d5d31e99a8f860a3956f6822
Morilence <1280659615@qq.com>
2022-11-04 19:03:16 +0800

    refactor(shared): change name for props 2 attrs

---
+++ packages/shared/src/type/attrs.ts
@@ -0,0 +1,154 @@
+import {
+    AnchorHTMLAttributes,
+    AreaHTMLAttributes,
+    AudioHTMLAttributes,
+    BaseHTMLAttributes,
+    BlockquoteHTMLAttributes,
+    ButtonHTMLAttributes,
+    CanvasHTMLAttributes,
+    ColgroupHTMLAttributes,
+    ColHTMLAttributes,
+    DataHTMLAttributes,
+    DelHTMLAttributes,
+    DialogHTMLAttributes,
+    EmbedHTMLAttributes,
+    FieldsetHTMLAttributes,
+    FormHTMLAttributes,
+    HTMLAttributes,
+    HtmlHTMLAttributes,
+    IframeHTMLAttributes,
+    ImgHTMLAttributes,
+    InputHTMLAttributes,
+    InsHTMLAttributes,
+    LabelHTMLAttributes,
+    LinkHTMLAttributes,
+    MapHTMLAttributes,
+    MetaHTMLAttributes,
+    MeterHTMLAttributes,
+    ObjectHTMLAttributes,
+    OlHTMLAttributes,
+    OptgroupHTMLAttributes,
+    OptionHTMLAttributes,
+    OutputHTMLAttributes,
+    ProgressHTMLAttributes,
+    QuoteHTMLAttributes,
+    ScriptHTMLAttributes,
+    SelectHTMLAttributes,
+    SourceHTMLAttributes,
+    StyleHTMLAttributes,
+    TableHTMLAttributes,
+    TdHTMLAttributes,
+    ThHTMLAttributes,
+} from "react";
+
+export type HTMLElementAttrs = HTMLAttributes<HTMLElement>;
+/** <a> */
+export type HTMLAnchorAttrs = AnchorHTMLAttributes<HTMLAnchorElement>;
+/** <area> */
+export type HTMLAreaAttrs = AreaHTMLAttributes<HTMLAreaElement>;
+/** <audio> */
+export type HTMLAudioAttrs = AudioHTMLAttributes<HTMLAudioElement>;
+/** <base> */
+export type HTMLBaseAttrs = BaseHTMLAttributes<HTMLBaseElement>;
+/** <blockquote> */
+export type HTMLBlockquoteAttrs = BlockquoteHTMLAttributes<HTMLQuoteElement>;
+/** <body> */
+export type HTMLBodyAttrs = HTMLAttributes<HTMLBodyElement>;
+/** <br> */
+export type HTMLBRAttrs = HTMLAttributes<HTMLBRElement>;
+/** <button> */
+export type HTMLButtonAttrs = ButtonHTMLAttributes<HTMLButtonElement>;
+/** <canvas>  */
+export type HTMLCanvasAttrs = CanvasHTMLAttributes<HTMLCanvasElement>;
+/** <col> */
+export type HTMLColAttrs = ColHTMLAttributes<HTMLTableColElement>;
+/** <colgroup> */
+export type HTMLColgroupAttrs = ColgroupHTMLAttributes<HTMLTableColElement>;
+/** <data> */
+export type HTMLDataAttrs = DataHTMLAttributes<HTMLDataElement>;
+/** <datalist> */
+export type HTMLDataListAttrs = HTMLAttributes<HTMLDataListElement>;
+/** <del> */
+export type HTMLDelAttrs = DelHTMLAttributes<HTMLModElement>;
+/** <dialog> */
+export type HTMLDialogAttrs = DialogHTMLAttributes<HTMLDialogElement>;
+/** <div> */
+export type HTMLDivAttrs = HTMLAttributes<HTMLDivElement>;
+/** <dl> */
+export type HTMLDListAttrs = HTMLAttributes<HTMLDListElement>;
+/** <embed> */
+export type HTMLEmbedAttrs = EmbedHTMLAttributes<HTMLEmbedElement>;
+/** <fieldset> */
+export type HTMLFieldSetAttrs = FieldsetHTMLAttributes<HTMLFieldSetElement>;
+/** <form> */
+export type HTMLFormAttrs = FormHTMLAttributes<HTMLFormElement>;
+/** <head> */
+export type HTMLHeadAttrs = HTMLAttributes<HTMLElement>;
+/** <h1> - <h6> */
+export type HTMLHeadingAttrs = HTMLAttributes<HTMLHeadingElement>;
+/** <hr> */
+export type HTMLHRAttrs = HTMLAttributes<HTMLHRElement>;
+/** <html> */
+export type HTMLHtmlAttrs = HtmlHTMLAttributes<HTMLHtmlElement>;
+/** <iframe> */
+export type HTMLIFrameAttrs = IframeHTMLAttributes<HTMLIFrameElement>;
+/** <img> */
+export type HTMLImageAttrs = ImgHTMLAttributes<HTMLImageElement>;
+/** <input> */
+export type HTMLInputAttrs = InputHTMLAttributes<HTMLInputElement>;
+/** <ins> */
+export type HTMLInsAttrs = InsHTMLAttributes<HTMLModElement>;
+/** <label> */
+export type HTMLLabelAttrs = LabelHTMLAttributes<HTMLLabelElement>;
+/** <legend> */
+export type HTMLLegendAttrs = HTMLAttributes<HTMLLegendElement>;
+/** <link> */
+export type HTMLLinkAttrs = LinkHTMLAttributes<HTMLLinkElement>;
+/** <map> */
+export type HTMLMapAttrs = MapHTMLAttributes<HTMLMapElement>;
+/** <meta> */
+export type HTMLMetaAttrs = MetaHTMLAttributes<HTMLMetaElement>;
+/** <meter> */
+export type HTMLMeterAttrs = MeterHTMLAttributes<HTMLMeterElement>;
+/** <object> */
+export type HTMLObjectAttrs = ObjectHTMLAttributes<HTMLObjectElement>;
+/** <ol> */
+export type HTMLOListAttrs = OlHTMLAttributes<HTMLOListElement>;
+/** <optgroup> */
+export type HTMLOptGroupAttrs = OptgroupHTMLAttributes<HTMLOptGroupElement>;
+/** <option> */
+export type HTMLOptionAttrs = OptionHTMLAttributes<HTMLOptionElement>;
+/** <output> */
+export type HTMLOutputAttrs = OutputHTMLAttributes<HTMLOutputElement>;
+/** <p> */
+export type HTMLParagraphAttrs = HTMLAttributes<HTMLParagraphElement>;
+/** <picture> */
+export type HTMLPictureAttrs = HTMLAttributes<HTMLElement>;
+/** <pre> */
+export type HTMLPreAttrs = HTMLAttributes<HTMLPreElement>;
+/** <progress> */
+export type HTMLProgressAttrs = ProgressHTMLAttributes<HTMLProgressElement>;
+/** <q> */
+export type HTMLQuoteAttrs = QuoteHTMLAttributes<HTMLQuoteElement>;
+/** <script> */
+export type HTMLScriptAttrs = ScriptHTMLAttributes<HTMLScriptElement>;
+/** <section> */
+export type HTMLSectionAttrs = HTMLAttributes<HTMLElement>;
+/** <select> */
+export type HTMLSelectAttrs = SelectHTMLAttributes<HTMLSelectElement>;
+/** <source> */
+export type HTMLSourceAttrs = SourceHTMLAttributes<HTMLSourceElement>;
+/** <span> */
+export type HTMLSpanAttrs = HTMLAttributes<HTMLSpanElement>;
+/** <style> */
+export type HTMLStyleAttrs = StyleHTMLAttributes<HTMLStyleElement>;
+/** <table> */
+export type HTMLTableAttrs = TableHTMLAttributes<HTMLTableElement>;
+/** <tbody> */
+export type HTMLTbodyAttrs = HTMLAttributes<HTMLTableSectionElement>;
+/** <td> */
+export type HTMLTdAttrs = TdHTMLAttributes<HTMLTableCellElement>;
+/** <th> */
+export type HTMLThAttrs = ThHTMLAttributes<HTMLTableCellElement>;
+/** <tr> */
+export type HTMLTrAttrs = HTMLAttributes<HTMLTableRowElement>;
