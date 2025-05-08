c9222384ba7cb48ae449dc0f05cbdbb97e038dbe
Morilence <1280659615@qq.com>
2025-05-08 17:56:57 +0800

    chore: update lint-staged settings

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
+    '*.vue': 'prettier --write',
     '*.{js,jsx,ts,tsx}': 'eslint --fix',
     '*.{json,json5,jsonc}': 'prettier --write',
 };
