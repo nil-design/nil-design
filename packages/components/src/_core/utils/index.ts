e2ae344b76874833da4537f86b37e1a7783bb8ed
Morilence <1280659615@qq.com>
2025-05-26 18:37:09 +0800

    refactor: extract common utils

--- packages/components/src/_core/utils/index.ts
+++ packages/components/src/_core/utils/index.ts
@@ -1,3 +1,6 @@
+import { isArray, isEmpty, isNil, isNumber, isString } from 'lodash-es';
+import { ReactNode, Fragment } from 'react';
+
 export const kebabToPascal = (text: string) => {
     if (text.includes('-')) {
         return text
@@ -6,4 +9,26 @@ export const kebabToPascal = (text: string) => {
             .join('');
     }
     return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+};
+
+export const isPlainChildren = (children: ReactNode): children is string | number => {
+    if (isNil(children)) return false;
+    if (isString(children) || isNumber(children)) return true;
+    if (isArray(children)) {
+        return children.every(child => isPlainChildren(child));
+    }
+    return false;
+};
+
+export const isEmptyChildren = (children: ReactNode): boolean => {
+    if (isNil(children)) return true;
+    if (isArray(children) && isEmpty(children)) return true;
+    if (
+        typeof children === 'object' &&
+        'type' in children &&
+        children.type === Fragment &&
+        isEmptyChildren(children.props.children)
+    )
+        return true;
+    return false;
 };
