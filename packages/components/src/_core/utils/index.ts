e46a2d34fb26e41a8d6a80162f0f8a06039ef330
Morilence <1280659615@qq.com>
2025-05-27 22:59:16 +0800

    feat(components/utils): clsx + twMerge

--- packages/components/src/_core/utils/index.ts
+++ packages/components/src/_core/utils/index.ts
@@ -1,34 +1,6 @@
-import { isArray, isEmpty, isNil, isNumber, isString } from 'lodash-es';
-import { ReactNode, Fragment } from 'react';
+import cn from './cn';
+import isEmptyChildren from './isEmptyChildren';
+import isPlainChildren from './isPlainChildren';
+import kebabToPascal from './kebabToPascal';
 
-export const kebabToPascal = (text: string) => {
-    if (text.includes('-')) {
-        return text
-            .split('-')
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-            .join('');
-    }
-    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
-};
-
-export const isPlainChildren = (children: ReactNode): children is string | number => {
-    if (isNil(children)) return false;
-    if (isString(children) || isNumber(children)) return true;
-    if (isArray(children)) {
-        return children.every(child => isPlainChildren(child));
-    }
-    return false;
-};
-
-export const isEmptyChildren = (children: ReactNode): boolean => {
-    if (isNil(children)) return true;
-    if (isArray(children) && isEmpty(children)) return true;
-    if (
-        typeof children === 'object' &&
-        'type' in children &&
-        children.type === Fragment &&
-        isEmptyChildren(children.props.children)
-    )
-        return true;
-    return false;
-};
+export { cn, isEmptyChildren, isPlainChildren, kebabToPascal };
