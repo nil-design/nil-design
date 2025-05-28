57976753dc7c6975c036d5954ea71347bfa8d113
Xavier.Meng <xavier.meng@fanruan.com>
2025-05-28 23:07:20 +0800

    fix(hooks): return type of useControllable

--- packages/hooks/src/useControllable.ts
+++ packages/hooks/src/useControllable.ts
@@ -2,13 +2,14 @@ import { isUndefined } from 'lodash-es';
 import { useState, Dispatch, SetStateAction } from 'react';
 import useUpdate from './useUpdate';
 
-const useControllable = <T>(controlledValue: T | undefined, defaultValue: T | (() => T)) => {
+const useControllable = <T>(
+    controlledValue: T | undefined,
+    defaultValue: T | (() => T),
+): [T, Dispatch<SetStateAction<T>>] => {
     const [uncontrolledValue, setUncontrolledValue] = useState<T>(defaultValue);
     const forceUpdate = useUpdate();
 
-    return !isUndefined(controlledValue)
-        ? [controlledValue, forceUpdate as Dispatch<SetStateAction<T>>]
-        : [uncontrolledValue, setUncontrolledValue];
+    return !isUndefined(controlledValue) ? [controlledValue, forceUpdate] : [uncontrolledValue, setUncontrolledValue];
 };
 
 export default useControllable;
