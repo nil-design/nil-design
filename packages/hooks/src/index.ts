392648e96e9cce8ed5e8b2287bdf14b4562fd74f
Morilence <1280659615@qq.com>
2025-06-04 18:31:54 +0800

    feat(hooks): useStableCallback

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,6 +1,7 @@
 import useControllable from './useControllable';
 import useLatest from './useLatest';
 import useLocalStorage from './useLocalStorage';
+import useStableCallback from './useStableCallback';
 import useUpdate from './useUpdate';
 
-export { useControllable, useLatest, useLocalStorage, useUpdate };
+export { useControllable, useLatest, useLocalStorage, useStableCallback, useUpdate };
