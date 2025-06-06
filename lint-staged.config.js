5c244b642fe3a9e6192021525815958ee4d2debd
Xavier.Meng <xavier.meng@fanruan.com>
2025-06-07 03:44:33 +0800

    chore: use stylelint 2 lint css

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,6 @@
 export default {
     '*.vue': 'prettier --write',
     '*.{js,jsx,ts,tsx}': 'eslint --fix',
+    '*.{css,less}': ['stylelint --fix', 'prettier --write'],
     '*.{json,json5,jsonc}': 'prettier --write',
 };
