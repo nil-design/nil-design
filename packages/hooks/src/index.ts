44308fb1d8a5590a70cf2564dc8dd8635ec85c4e
Morilence <1280659615@qq.com>
2025-06-16 09:49:22 +0800

    fix(hooks): exposed usePreviousRef

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -3,6 +3,7 @@ import useForceUpdate from './use-force-update';
 import useIsomorphicLayoutEffect from './use-isomorphic-layout-effect';
 import useLatestRef from './use-latest-ref';
 import useLocalStorage from './use-local-storage';
+import usePreviousRef from './use-previous-ref';
 import useStableCallback from './use-stable-callback';
 
 export {
@@ -11,5 +12,6 @@ export {
     useIsomorphicLayoutEffect,
     useLatestRef,
     useLocalStorage,
+    usePreviousRef,
     useStableCallback,
 };
