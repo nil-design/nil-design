dfbe47743ab29d812fb6b998d629c79a3443e68b
Xavier.Meng <xavier.meng@fanruan.com>
2025-06-22 22:07:16 +0800

    feat(shared): add Optional util type

--- packages/shared/src/interfaces/index.d.ts
+++ packages/shared/src/interfaces/index.d.ts
@@ -1,5 +1,6 @@
 import type CSSPropertiesWithVars from './css-properties-with-vars';
+import type Optional from './optional';
 import type PossibleRef from './possible-ref';
 import type Primitive from './primitive';
 
-export type { CSSPropertiesWithVars, PossibleRef, Primitive };
+export type { CSSPropertiesWithVars, Optional, PossibleRef, Primitive };
