d762bd58d3cbece85fc650c2d79fc81016ea7719
Xavier.Meng <xavier.meng@fanruan.com>
2025-06-26 00:14:28 +0800

    feat(shared): UnionToIntersection

--- packages/shared/src/interfaces/index.d.ts
+++ packages/shared/src/interfaces/index.d.ts
@@ -2,5 +2,6 @@ import type CSSPropertiesWithVars from './css-properties-with-vars';
 import type Optional from './optional';
 import type PossibleRef from './possible-ref';
 import type Primitive from './primitive';
+import type UnionToIntersection from './union-to-intersection';
 
-export type { CSSPropertiesWithVars, Optional, PossibleRef, Primitive };
+export type { CSSPropertiesWithVars, Optional, PossibleRef, Primitive, UnionToIntersection };
