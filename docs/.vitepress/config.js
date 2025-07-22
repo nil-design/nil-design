ec77444d255ec1593a5d3e6a59f3b86e052a2620
Morilence <1280659615@qq.com>
2025-07-22 18:02:34 +0800

    docs: add sitemap

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -24,6 +24,9 @@ export default defineConfig({
             },
         ],
     ],
+    sitemap: {
+        hostname: 'https://nil-design.github.io/nil-design/',
+    },
     lastUpdated: true,
     themeConfig: {
         logo: '/logo.svg',
