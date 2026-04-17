4e1e7eac24ca0cdcf5d768bcdfd4da670b3381c0
Morilence <1280659615@qq.com>
2026-04-17 16:56:39 +0800

    chore: enforce curly braces in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -37,6 +37,7 @@ export default tslint.config(
             },
         },
         rules: {
+            curly: ['warn', 'all'],
             'no-console': 'warn',
             'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
             'padding-line-between-statements': [
